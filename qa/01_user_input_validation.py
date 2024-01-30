6e74edb5817f55d93b12ccddedf1e7e3682802ba
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-01-30 10:17:20 -0300

    01_user_input_validation.py validated

--- qa/01_user_input_validation.py
+++ qa/01_user_input_validation.py
@@ -22,26 +22,19 @@ max_on_list[4] = str(int_max + 1)
 stdout_list = []
 stderr_list = []
 args = [ordered_list, str_on_list, dup_on_list, min_on_list, max_on_list, nothing_on_list]
-stderr_ref_list = ["b''", "b'Error\n'", "b'Error\n'", "b'Error\n'", "b'Error\n'", "b''"]
-stdout_ref_list = ["b''", "b''", "b''", "b''", "b''", "b''"]
+stderr_ref_list = ["", "Error\n", "Error\n", "Error\n", "Error\n", ""]
+stdout_ref_list = ["", "", "", "", "", ""]
 for arg in args:
 	arg.insert(0, cmd0)
-	output = subprocess.run(arg, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	output = subprocess.run(arg, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 	stderr_list.append(output.stderr)
 	stdout_list.append(output.stdout)
 
-print(stderr_list)
-print(stdout_list)
-
 i = 0
 for out, err in zip(stdout_list, stderr_list):
-	if ((out == stdout_ref_list[i])):
+	if ((out == stdout_ref_list[i]) and (err == stderr_ref_list[i])):
 		print(f"{i + 1}. OK\n")
 	else:
 		print(f"{i + 1}. KO\n")
+	print(out, stdout_ref_list[i])
 	i = i + 1
-
-i = 0
-for out, err in zip(stdout_list, stderr_list):
-	print(f"out:{out} | err:{err}")
-	print(f"out_ref:{stdout_ref_list[i]} | err_ref:{stderr_ref_list[i]}\n")
