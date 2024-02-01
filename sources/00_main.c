08bb5d835747595949a76c09f6a0068ee6d1c827
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-02-01 19:04:34 -0300

    indexing test ready

--- sources/00_main.c
+++ sources/00_main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   00_main.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
+/*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 10:52:11 by josfelip          #+#    #+#             */
-/*   Updated: 2024/01/31 18:00:17 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/02/01 19:00:18 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@ int main(int argc, char *argv[])
 	
 	bst = NULL;
 	ft_user_input_validation(argc, argv, &bst);
+	bst_in_order(bst);
 	bst_free_all(bst);
 }
