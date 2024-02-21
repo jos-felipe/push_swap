e150891ac5b85fbae668aef07c3584cd4b1dc503
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-02-21 12:32:17 -0300

    simple version tester updated with checker; running the checker via python works.

--- qa/evaluator_modules/_03_simple_version.py
+++ qa/evaluator_modules/_03_simple_version.py
@@ -3,27 +3,27 @@ import subprocess
 def	simple_version(program, valgrind, valgrind_check, colours):
 
 	# Definig args
-	all_tiny_sort_args = [["1", "3", "2"], ["2", "1", "3"], ["2", "3", "1"], ["3", "1", "2"], ["3", "2", "1"]]
-
-	# Defining references
-	move_ref_list = ["rra\nsa\n", "sa\n", "rra\n", "ra\n", "ra\nsa\n"]
+	all_tiny_sort_args_checker = ["'1 3 2'", "'2 1 3'", "'2 3 1'", "'3 1 2'", "'3 2 1'"]
+	all_tiny_sort_args_val = [["1", "3", "2"], ["2", "1", "3"], ["2", "3", "1"], ["3", "1", "2"], ["3", "2", "1"]]
+	ref_list = ["OK\n", "OK\n", "OK\n", "OK\n", "OK\n"]
 
 	# Colecting outputs
 	all_outputs = []
 	all_outputs_val = []
-	for tiny_sort_arg in all_tiny_sort_args:
+	for tiny_sort_arg, all_tiny_sort_arg_val in zip(all_tiny_sort_args_checker, all_tiny_sort_args_val):
 
-		cmd = program + tiny_sort_arg
-		output = subprocess.run(cmd, stdout=subprocess.PIPE, text=True)
+		arg = f"ARG={tiny_sort_arg}; ../push_swap $ARG | ../checker_linux $ARG"
+		output = subprocess.run(arg, stdout=subprocess.PIPE, text=True, shell=True)
 		all_outputs.append(output.stdout)
 
+		cmd = program + all_tiny_sort_arg_val
 		cmd_val = valgrind + cmd
 		output_val = subprocess.run(cmd_val, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 		all_outputs_val.append(output_val.stderr)
 	
 	# Checking ref with outputs
 	i = 1
-	for out, out_val, out_ref in zip(all_outputs, all_outputs_val, move_ref_list):
+	for out, out_val, out_ref in zip(all_outputs, all_outputs_val, ref_list):
 		print("---------")
 		if (out == out_ref):
 			print(f"{colours[0]}{i}. OK  {colours[2]}")
