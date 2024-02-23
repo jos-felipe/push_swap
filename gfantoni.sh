bac46f3d4fdc12161d3897cb63a764b78525d666
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-02-23 14:15:07 -0300

    josfelip.sh and gfantoni.sh

--- gfantoni.sh
+++ gfantoni.sh
@@ -1,3 +1,11 @@
-!#/bin/bash
+#!/bin/bash
 
 find . sources lib includes \( -name "*.c" -or -name "*.h" -or -name Makefile \) -exec sed -e 's/josfelip/gfantoni/; s/<josfelip@student.42sp.org.br>/<gfantoni@student.42sp.org.br>/' {} --in-place \;
+
+rm -rf .github
+rm -rf .gitignore
+rm -rf qa
+rm README.md
+rm LICENSE
+rm gfantoni.sh
+rm josfelip.sh
